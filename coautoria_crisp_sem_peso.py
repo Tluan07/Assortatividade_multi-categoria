b9e39c73cd292615a44a43d5ecfc4e5c2c701e7d
Tluan07 <thiagolinhares636@gmail.com>
2026-09-11 10:49:54 -0300

    Update base path for data files

--- coautoria_crisp_sem_peso.py
+++ coautoria_crisp_sem_peso.py
@@ -22,7 +22,7 @@ import networkx as nx
 
 from assortatividade import s_min, s_max, s_jaccard, s_dir, calcular_r
 
-CAMINHO_BASE = './dados/wos'
+CAMINHO_BASE = '/content/sample_data/'
 TOPICOS = {
     'History of Probability': os.path.join(CAMINHO_BASE, 'hist_of_prob'),
     'Cryptography':           os.path.join(CAMINHO_BASE, 'cryptography'),
